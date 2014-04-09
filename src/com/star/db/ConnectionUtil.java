48cb8b8a2e2e2f22c5b3e29f0c7dfce520a43292
hiwangmx <hiwangmx@gmail.com>
2014-04-09 08:29:21 +0800

    23

--- src/com/star/db/ConnectionUtil.java
+++ src/com/star/db/ConnectionUtil.java
@@ -2,4 +2,8 @@ package com.star.db;
 
 public class ConnectionUtil {
 
+	private final static String CONN_URl = "";
+	private final static String USER_NAME = "";
+	private final static String PASSWORD = "";
+	
 }
